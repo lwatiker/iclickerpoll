634dc98910968bf68fd6bf8272245356c7998de8
Laura Watiker <lwatiker@gmail.com>
2014-04-27 17:04:16 -0400

    Added in initial TKinter framework

--- iclickerpoll.py
+++ iclickerpoll.py
@@ -7,9 +7,31 @@ from array import array
 from collections import defaultdict, Counter
 import logging, time, sys
 import threading
+import Tkinter
 
 log = logging.getLogger(__name__)
 
+class sniff_tk(Tkinter.Tk):
+    def __init__(self,parent):
+        Tkinter.Tk.__init__(self,parent)
+        self.parent = parent
+        self.initialize()
+
+    def initialize(self):
+        self.grid()
+
+        button = Tkinter.Button(self,text=u"Initialize Base")
+        button.grid(column=0,row=0)
+
+        button = Tkinter.Button(self,text=u"Start Poll")
+        button.grid(column=0,row=1)
+
+        button = Tkinter.Button(self,text=u"End Poll")
+        button.grid(column=0,row=2)
+
+
+        self.grid_columnconfigure(0,weight=1)
+        self.resizable(True,False)
 
 class Command(object):
     def __init__(self, byte_array=None):
@@ -447,64 +469,68 @@ def close_pole(poll):
 if __name__ == '__main__':
     import signal, argparse
 
-    parser = argparse.ArgumentParser(description='Start an iClicker poll')
-    parser.add_argument('--debug', action='store_true', default=False,
-                        help='Display debug information about the USB transactions')
-    parser.add_argument('--type', type=str, default='alpha',
-                        help='Sets the poll type to alpha, numeric, or alphanumeric')
-    parser.add_argument('--duration', type=str, default='100m0s',
-                        help='Sets the duration of the poll in minutes and seconds. 0m0s is unlimited.')
-    parser.add_argument('--dest', type=str, default='',
-                        help='Sets the file to save polling data to.')
-    parser.add_argument('--frequency', type=str, default='aa',
-                        help='Sets the two base-station frequency codes. Should be formatted as two letters (e.g., \'aa\' or \'ab\')')
+    gui = sniff_tk(None)
+    gui.title('Sniffer')
+    gui.mainloop()
 
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(description='Start an iClicker poll')
+    # parser.add_argument('--debug', action='store_true', default=False,
+    #                     help='Display debug information about the USB transactions')
+    # parser.add_argument('--type', type=str, default='alpha',
+    #                     help='Sets the poll type to alpha, numeric, or alphanumeric')
+    # parser.add_argument('--duration', type=str, default='100m0s',
+    #                     help='Sets the duration of the poll in minutes and seconds. 0m0s is unlimited.')
+    # parser.add_argument('--dest', type=str, default='',
+    #                     help='Sets the file to save polling data to.')
+    # parser.add_argument('--frequency', type=str, default='aa',
+    #                     help='Sets the two base-station frequency codes. Should be formatted as two letters (e.g., \'aa\' or \'ab\')')
 
-    #
-    # Process all the arguments
-    #
+    # args = parser.parse_args()
 
-    if args.debug:
-        log.setLevel(0)
-    if args.type in ['alpha', 'numeric', 'alphanumeric']:
-        poll_type = args.type
-    else:
-        raise ValueError("Poll type must be 'alpha', 'numeric', or 'alphanumeric', not '{0}'".format(args.type))
-    if args.duration:
-        poll_duration = 1000
-    if args.frequency:
-        freq1 = args.frequency[0].lower()
-        freq2 = args.frequency[1].lower()
-        if freq1 not in ('a', 'b', 'c', 'd') or freq2 not in ('a', 'b', 'c', 'd'):
-            raise ValueError("Frequency combintation '{0}{1}' is not valid".format(freq1, freq2))
+    # #
+    # # Process all the arguments
+    # #
+
+    # if args.debug:
+    #     log.setLevel(0)
+    # if args.type in ['alpha', 'numeric', 'alphanumeric']:
+    #     poll_type = args.type
+    # else:
+    #     raise ValueError("Poll type must be 'alpha', 'numeric', or 'alphanumeric', not '{0}'".format(args.type))
+    # if args.duration:
+    #     poll_duration = 1000
+    # if args.frequency:
+    #     freq1 = args.frequency[0].lower()
+    #     freq2 = args.frequency[1].lower()
+    #     if freq1 not in ('a', 'b', 'c', 'd') or freq2 not in ('a', 'b', 'c', 'd'):
+    #         raise ValueError("Frequency combintation '{0}{1}' is not valid".format(freq1, freq2))
     
 
-    #
-    # Initiate the polling
-    #
-    print('Finding iClicker Base')
-    base = IClickerBase()
-    base.get_base()
-    print('Initializing iClicker Base')
-    base.initialize(freq1, freq2)
+    # #
+    # # Initiate the polling
+    # #
+    # print('Finding iClicker Base')
+    # base = IClickerBase()
+    # base.get_base()
+    # print('Initializing iClicker Base')
+    # base.initialize(freq1, freq2)
         
-    # If we have successfully started a poll, set up a signal handler
-    # to clean up when we get a SIGINT (ctrl+c or kill) command
-    poll = IClickerPoll(base)
-    signal.signal(signal.SIGINT, lambda *x: close_pole(poll))
-    # Set a callback to stop the poll after the desired amount of time
-    if poll_duration:
-        stop_timer = threading.Timer(poll_duration, lambda *x: close_pole(poll))
-        stop_timer.start()
-    print('Poll Started')
-    poll.start_poll(poll_type)
+    # # If we have successfully started a poll, set up a signal handler
+    # # to clean up when we get a SIGINT (ctrl+c or kill) command
+    # poll = IClickerPoll(base)
+    # signal.signal(signal.SIGINT, lambda *x: close_pole(poll))
+    # # Set a callback to stop the poll after the desired amount of time
+    # if poll_duration:
+    #     stop_timer = threading.Timer(poll_duration, lambda *x: close_pole(poll))
+    #     stop_timer.start()
+    # print('Poll Started')
+    # poll.start_poll(poll_type)
 
-    # If we made it this far and stop_timer wasn't triggered, we were asked to stop another
-    # way, so we should stop the stop_timer
-    stop_timer.cancel()
-    if args.dest:
-        file_name = args.dest
-        print('Writing results to {0}'.format(file_name))
-        with open(file_name, 'w') as out_file:
-            out_file.write(poll.get_most_recent_responses_formatted())
+    # # If we made it this far and stop_timer wasn't triggered, we were asked to stop another
+    # # way, so we should stop the stop_timer
+    # stop_timer.cancel()
+    # if args.dest:
+    #     file_name = args.dest
+    #     print('Writing results to {0}'.format(file_name))
+    #     with open(file_name, 'w') as out_file:
+    #         out_file.write(poll.get_most_recent_responses_formatted())
